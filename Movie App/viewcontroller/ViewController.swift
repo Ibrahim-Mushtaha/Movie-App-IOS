5c93ed82b6306a0dc143f7bae5e1434d99c04595
Ibrahim-Mushtaha <ibrahim.mushtaha2@gmail.com>
2021-05-27 03:33:48 -0700

    add Tab bar & navigation bar

---
+++ Movie App/viewcontroller/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Ibrahim Mushtaha on 25/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
+extension UIView {
+    func dropShadow(scale: Bool = true) {
+        layer.masksToBounds = true
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 1
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+    
+    func addCorner(value:Int = 4){
+        layer.masksToBounds = true
+        layer.cornerRadius = 4
+    }
+}
+
+
