c3205b8e8f5987a1614f9e6ac8a26bde7539d1e5
Ibrahim-Mushtaha <ibrahim.mushtaha2@gmail.com>
2021-05-27 05:29:49 -0700

    add image slider

---
+++ Movie App/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  Movie App
+//
+//  Created by Ibrahim Mushtaha on 27/05/2021.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var uiImageView: UIImageView!
+    
+    
+}
---
+++ Movie App/viewcontroller/HomeViewController.swift
@@ -0,0 +1,59 @@
+//
+//  HomeViewController.swift
+//  Movie App
+//
+//  Created by Ibrahim Mushtaha on 27/05/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var uiSliderView: UICollectionView!
+    
+    var timer:Timer?
+    var currentCellIndex = 0
+    var webserviceImage = ["image2","image1"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(slideToNext), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func slideToNext(){
+        if currentCellIndex < webserviceImage.count - 1 {
+            currentCellIndex = currentCellIndex + 1
+        } else{
+           currentCellIndex = 0
+        }
+        
+        uiSliderView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .right, animated: true)
+    }
+    
+
+}
+
+extension HomeViewController : UICollectionViewDelegate , UICollectionViewDataSource ,UICollectionViewDelegateFlowLayout{
+    
+func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return webserviceImage.count
+}
+
+func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell = uiSliderView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+    
+    cell.uiImageView.image = UIImage(named: webserviceImage[indexPath.row])
+    
+    return cell
+}
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (uiSliderView.bounds.size.width/2) - 12, height:(uiSliderView.bounds.size.height/3) - 20)
+    }
+    
+}
+
