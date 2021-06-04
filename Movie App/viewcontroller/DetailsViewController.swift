c714f2c323c5e881783a89aaa2e998831c4c7d46
Ibrahim-Mushtaha <ibrahim.mushtaha2@gmail.com>
2021-06-04 12:13:31 -0700

    add remove from favorite

--- Movie App/viewcontroller/DetailsViewController.swift
+++ Movie App/viewcontroller/DetailsViewController.swift
@@ -26,6 +26,8 @@ class DetailsViewController: UIViewController {
     
     var onChange:DataChange!
     
+    var isAdded = false
+    
     
     override func viewWillAppear(_ animated: Bool) {
         
@@ -34,25 +36,51 @@ class DetailsViewController: UIViewController {
             DispatchQueue.main.async {
             if let movie = self.movie{
                 for favorite in favorites {
-                    if((favorite as! Favorite).original_title == movie.original_title ){
+                    let favObject = favorite as! Favorite
+                    if(favObject.original_title == movie.original_title ){
                         print("movie is add in data core")
                         self.uiFavoriteImage.imageView?.image = UIImage(named: "star.fill")
                         self.uiFavoriteImage.imageView?.tintColor = UIColor.red
+                        self.isAdded = true
+                       /* self.favorite = Favorite(context: self.context)
+                        self.favorite?.movie_id = favObject.movie_id
+                        self.favorite?.overview = favObject.overview
+                        self.favorite?.original_title = favObject.original_title
+                        self.favorite?.poster_path = favObject.poster_path
+                        self.favorite?.release_date = favObject.release_date
+ */
                     }
                 }
+                
+        
+                
             }else {
                 for favorite in favorites {
                     if((favorite as! Favorite).original_title == self.favorite?.original_title){
                         print("favorite movie is add in data core")
                         self.uiFavoriteImage.imageView?.image = UIImage(named: "star.fill")
                         self.uiFavoriteImage.imageView?.tintColor = UIColor.red
+                        self.isAdded = true
                     }
                 }
+            
+                if self.favorite == nil {
+                    self.favorite = Favorite(context: self.context)
+                    self.favorite?.movie_id = self.favorite?.movie_id
+                    self.favorite?.overview = self.favorite?.overview
+                    self.favorite?.original_title = self.favorite?.original_title
+                    self.favorite?.poster_path = self.favorite?.poster_path
+                    self.favorite?.release_date = self.favorite?.release_date
+                }
+            
             }
         }
         } catch let error {
             print(error.localizedDescription)
         }
+        
+    
+        
     }
     
     override func viewDidLoad() {
@@ -82,26 +110,43 @@ class DetailsViewController: UIViewController {
     
 
     @IBAction func uiFavoritebtn(_ sender: UIButton) {
-        if favorite == nil {
-            favorite = Favorite(context: self.context)
-        }
-        
-        favorite?.movie_id = "\(String(describing: movie?.id))"
-        favorite?.original_title = movie?.original_title
-        favorite?.overview = movie?.overview
-        favorite?.poster_path = movie?.poster_path
-        favorite?.release_date = movie?.release_date
-        
-        do{
-            try self.context.save()
-            DispatchQueue.main.async {
-                    self.onChange.onChangeData()
+    
+        if(isAdded){
+        uiFavoriteImage.imageView?.image = UIImage(named: "star.fill")
+        uiFavoriteImage.imageView?.tintColor = UIColor.red
+            deleteFromCoreData(favorite: self.favorite!)
+        }else {
+            if favorite == nil {
+                favorite = Favorite(context: self.context)
             }
+            
+            favorite?.movie_id = "\(String(describing: movie?.id))"
+            favorite?.original_title = movie?.original_title
+            favorite?.overview = movie?.overview
+            favorite?.poster_path = movie?.poster_path
+            favorite?.release_date = movie?.release_date
+            
+            do{
+                try self.context.save()
+                DispatchQueue.main.async {
+                        self.onChange.onChangeData()
+                }
+            }catch{
+                
+            }
+        }
+    }
+    
+    
+    func deleteFromCoreData(favorite:Favorite){
+        let lineToRemove = favorite
+        do{
+            try self.context.delete(lineToRemove)
+            self.dismiss(animated: true, completion: nil)
+            self.onChange.onChangeData()
         }catch{
             
         }
-        uiFavoriteImage.imageView?.image = UIImage(named: "star.fill")
-        uiFavoriteImage.imageView?.tintColor = UIColor.red
     }
 }
 
--- Movie App/viewcontroller/FavoriteViewController.swift
+++ Movie App/viewcontroller/FavoriteViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class FavoriteViewController: UIViewController, DataChange {
     func onChangeData() {
-        
+        fetchData()
     }
     
     
